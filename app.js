4873c4a5577d0022f6608eb521632041195113c1
M0unir <M0unir@users.noreply.github.com>
2015-12-30 16:37:33 -0800

    Added upvotes, creating posts

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
 var app = angular.module('RedditNews', []);
 
 app.controller('MainCtrl', ['$scope', function ($scope) {
-  $scope.hello = 'Hello world!';
+  $scope.heading = 'Reddit Clone';
 
-  $scope.posts = [
-  {title: 'post 1', upvotes: 5},
-  {title: 'post 2', upvotes: 2},
-  {title: 'post 3', upvotes: 15},
-  {title: 'post 4', upvotes: 9},
-  {title: 'post 5', upvotes: 4}
-  ];
+  $scope.posts = [{
+    title: 'post 1',
+    upvotes: 5
+  }, {
+    title: 'post 2',
+    upvotes: 2
+  }, {
+    title: 'post 3',
+    upvotes: 15
+  }, {
+    title: 'post 4',
+    upvotes: 9
+  }, {
+    title: 'post 5',
+    upvotes: 4
+  }];
 
+  //Add Post
+  $scope.addPost = function () {
+    if (!$scope.title || $scope.title === '') {
+      return;
+    }
+    $scope.posts.push({
+      title: $scope.title,
+      link: $scope.link,
+      upvotes: 0
+    });
+    $scope.title = '';
+    $scope.link = '';
+  };
+
+  //Increment upvotes
+  $scope.incrementUpvotes = function (post) {
+    post.upvotes += 1;
+  }
 
 }]);
